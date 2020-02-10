502a15fff30de6a3aab523514e31518f580cedc9
Michael Liu <mike666234@gmail.com>
2020-02-10 00:22:42 -0800

    Add navbar, login button, login popover

--- testbank/src/App.js
+++ testbank/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
+
+import Typography from '@material-ui/core/Typography'
+import CssBaseline from '@material-ui/core/CssBaseline'
 import Button from '@material-ui/core/Button'
+
 import './App.css'
+
+import ButtonAppBar from './ButtonAppBar'
 
 const apiUrl = `http://localhost:8080`
 
@@ -32,22 +38,35 @@ class App extends React.Component {
   }
   
   render() {
+    // return (
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <Button variant="contained" color="primary">
+    //         Hello World
+    //       </Button>
+    //       <button onClick={() => this.createUser()}>Create User</button>
+    //       <p>Users liast:</p>
+    //       <ul>
+    //         {this.state.users.map(user => (
+    //           <li key={user._id}>id: {user._id}</li>
+    //         ))}
+    //       </ul>
+    //     </header>
+    //   </div>
+    // )
     return (
-      <div className="App">
-        <header className="App-header">
-          <Button variant="contained" color="primary">
-            Hello World
-          </Button>
-          <button onClick={() => this.createUser()}>Create User</button>
-          <p>Users liast:</p>
-          <ul>
-            {this.state.users.map(user => (
-              <li key={user._id}>id: {user._id}</li>
-            ))}
-          </ul>
-        </header>
-      </div>
+      <>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <CssBaseline />
+        <ButtonAppBar />
+        <div className="App">
+          <Typography variant="h1" component="h2" gutterBottom>
+            h1. Heading
+          </Typography>
+        </div>
+      </>
     )
+    
   }
 }
 
