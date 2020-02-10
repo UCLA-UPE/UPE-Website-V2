f13b60b5ad312a4e34bf81e4f980aca0cc9a7372
Michael Liu <mike666234@gmail.com>
2020-02-09 18:34:39 -0800

    Add template file for html-webpack-plugin, docker-compose works

--- testbank/webpack.config.js
+++ testbank/webpack.config.js
@@ -1,34 +1,38 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: ["@babel/polyfill", "./src/index.js"],
-  mode: "development",
+  entry: ['@babel/polyfill', './src/index.js'],
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env", "@babel/preset-react"] }
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env', '@babel/preset-react'] }
       },
       {
         test: /\.css$/,
         exclude: /(node_modules|bower_components)/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/',
+    filename: 'bundle.js'
   },
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
+    contentBase: path.join(__dirname, 'public/'),
     port: 3000,
-    // publicPath: "http://localhost:3000/dist/",
-    publicPath: "/dist/",
-  }
+    // publicPath: 'http://localhost:3000/dist/',
+    publicPath: '/dist/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' })
+  ]
 };
