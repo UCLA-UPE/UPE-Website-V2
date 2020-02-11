0745a08982d1109d005038d6b9fcdb5601056d01
Michael Liu <mike666234@gmail.com>
2020-02-10 19:15:37 -0800

    Remove passport middleware to reduce code bloat

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ const connectDb = require('./src/connection')
 const cors = require('cors')
 const bcrypt = require('bcrypt')
 const passport = require('passport')
-const localStrategy = require('passport-local').Strategy
 const jwt = require('jsonwebtoken')
 
 // some magic constants
@@ -29,35 +28,6 @@ app.use(passport.initialize())
 
 // passport
 // https://www.digitalocean.com/community/tutorials/api-authentication-with-json-web-tokensjwt-and-passport
-
-passport.use('signup', new localStrategy({
-  usernameField: 'email',
-  passwordField: 'password',
-  session: false
-}, async (email, password, done) => {
-  const exists = await User.findOne({ email })
-  if (exists) {
-    return done(null, false, { message: 'Email exists'})
-  }
-  const user = await User.create({ email, password })
-  return done(null, user)
-}))
-
-passport.use('login', new localStrategy({
-  usernameField: 'email',
-  passwordField: 'password',
-  session: false
-}, async (email, password, done) => {
-  const user = await User.findOne({ email })
-  if (!user) {
-    return done(null, false, { message: 'User not found' })
-  }
-  const validate = await user.isValidPassword(password)
-  if (!validate) {
-    return done(null, false, { message: 'Wrong Password' })
-  }
-  return done(null, user)
-}))
 
 
 
@@ -83,34 +53,28 @@ app.get('/user-create', async (req, res) => {
   res.send('User created \n')
 })
 
-// use passport custom callback to return proper json
-app.post('/login', (req, res, next) => {
-  passport.authenticate('login', { 'session': false }, async (err, user, info) => {
-    if (err) { return next(err) }
-    if (!user) {
-      res.sendStatus(401)
-      return
-    }
-    const token = await jwt.sign(
-      { _id: user._id, email: user.email }, 
-      SECRET_PRIVATE_KEY, 
-      { 'expiresIn': '10s' }
-    )
-    res.status(200).json({ token })
-  })(req, res, next)
+app.post('/login', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email })
+  if (!user || ! await user.isValidPassword(req.body.password)) {
+    res.sendStatus(401)
+    return
+  }
+  const token = await jwt.sign(
+    { _id: user._id, email: user.email }, 
+    SECRET_PRIVATE_KEY, 
+    { 'expiresIn': '10s' }
+  )
+  res.status(200).json({ token })
 })
 
-// use passport custom callback to return proper json
-app.post('/signup', (req, res, next) => {
-  passport.authenticate('signup', { 'session': false }, async (err, user, info) => {
-    if (err) { return next(err) }
-    if (!user) {
-      res.sendStatus(401)
-      return
-    }
-    console.log('New user created: ' + user)
-    res.sendStatus(200)
-  })(req, res, next)
+app.post('/signup', async (req, res) => {
+  const exists = await User.findOne({ email: req.body.email })
+  if (exists) {
+    res.sendStatus(401)
+    return
+  }
+  await User.create({ email: req.body.email, password: req.body.password })
+  res.sendStatus(200)
 })
 
 
